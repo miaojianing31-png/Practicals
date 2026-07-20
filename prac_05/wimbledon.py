8fcce8254449ec55702a32ec50aa5ae3dfb41041
miaojianing <miaojianing31@gmail.com>
2026-07-20 18:24:50 +0800

    Complete Wimbledon results display

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -8,19 +8,14 @@ FILENAME = "wimbledon.csv"
 
 
 def main():
-    """Read and process Wimbledon data."""
+    """Read, process and display Wimbledon data."""
     records = read_data(FILENAME)
     champion_to_wins, countries = process_data(records)
-    print(champion_to_wins)
-    print(countries)
-
-    """Read and display Wimbledon data."""
-    records = read_data(FILENAME)
-    print(records)
+    display_results(champion_to_wins, countries)
 
 
 def read_data(filename):
-    """Read Wimbledon CSV data."""
+    """Read Wimbledon CSV data and return a list of records."""
     records = []
 
     with open(filename, "r", encoding="utf-8-sig", newline="") as in_file:
@@ -31,6 +26,7 @@ def read_data(filename):
             records.append(row)
 
     return records
+
 
 def process_data(records):
     """Create champion win counts and champion countries."""
@@ -45,5 +41,20 @@ def process_data(records):
         countries.add(country)
 
     return champion_to_wins, countries
+
+
+def display_results(champion_to_wins, countries):
+    """Display Wimbledon champions and countries."""
+    print("Wimbledon Champions:")
+
+    for champion, number_of_wins in champion_to_wins.items():
+        print(f"{champion} {number_of_wins}")
+
+    sorted_countries = sorted(countries)
+
+    print()
+    print(f"These {len(sorted_countries)} countries have won Wimbledon:")
+    print(", ".join(sorted_countries))
+
 
 main()
