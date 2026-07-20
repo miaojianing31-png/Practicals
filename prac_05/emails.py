039599000573917521c85989dcae48924d4fd177
miaojianing <miaojianing31@gmail.com>
2026-07-20 17:35:34 +0800

    Complete email and name confirmation

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -3,6 +3,26 @@ CP1404 Practical 05 - Emails
 """
 
 
+def main():
+    """Store emails and associated names."""
+    email_to_name = {}
+
+    email = input("Email: ").strip()
+    while email != "":
+        name = extract_name(email)
+        confirmation = input(f"Is your name {name}? (Y/n) ").strip().lower()
+
+        if confirmation not in ("", "y"):
+            name = input("Name: ").strip()
+
+        email_to_name[email] = name
+        email = input("Email: ").strip()
+
+    print()
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
+
+
 def extract_name(email):
     """Extract a name from an email address."""
     email_name = email.split("@")[0]
@@ -10,13 +30,4 @@ def extract_name(email):
     return " ".join(name_parts).title()
 
 
-email_to_name = {}
-
-email = input("Email: ").strip()
-while email != "":
-    name = extract_name(email)
-    email_to_name[email] = name
-    email = input("Email: ").strip()
-
-for email, name in email_to_name.items():
-    print(f"{name} ({email})")
+main()
