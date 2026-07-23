bf40b3b210e8c1f9ac0425204e708ff6fb2b6960
miaojianing <miaojianing31@gmail.com>
2026-07-23 11:02:43 +0800

    Count word occurrences using a dictionary

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,18 @@
+"""
+CP1404 Practical 05 - Word Occurrences
+
+"""
+
+text = input("Text: ")
+words = text.split()
+
+word_to_count = {}
+
+for word in words:
+    if word in word_to_count:
+        word_to_count[word] += 1
+    else:
+        word_to_count[word] = 1
+
+for word, count in word_to_count.items():
+    print(word, ":", count)
